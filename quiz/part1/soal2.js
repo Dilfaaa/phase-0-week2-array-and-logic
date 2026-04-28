6f7dd796b51720eb1a22e6a3fe182ca7c5c9ab9e
FadilYusril <yusrilneneknu51@gmail.com>
2026-04-28 08:07:28 +0800

    part 1 nya ketinggalan sir

---
+++ quiz/part1/soal2.js
@@ -0,0 +1,80 @@
+// ## Soal 2 
+// pada soal yg kedua, kalian harus belajar method `splice`, `slice`, `join`, 'split', dan lainnya
+let input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+
+function dataHandling2(input) {
+// keluaran pertama
+input.splice(1,1, "Roman Alamsyah Elsharawy")
+input.splice(2,1, "Provinsi Bandar Lampung")
+input.splice(4,1,"Pria")
+input.splice(5,0, "SMA Internasional Metro")
+console.log(input)
+
+//membagi ttl
+let ttl = input[3].split("/")
+let bulan = parseInt(ttl[1])
+// nama2 bulan
+let namaBulan = ""
+switch (bulan) {
+    case 1:
+        namaBulan = "Januari"
+    break
+    case 2:
+        namaBulan = "Februari"
+    break
+    case 3:
+        namaBulan = "Maret"
+    break
+    case 4:
+        namaBulan = "April"
+    break
+    case 5:
+        namaBulan = "Mei"
+    break
+    case 6:
+        namaBulan = "Juni"
+    break
+    case 7:
+        namaBulan = "Juli"
+    break
+    case 8:
+        namaBulan = "Agustus"
+    break
+    case 9:
+        namaBulan = "September"
+    break
+    case 10:
+        namaBulan = "Oktober"
+    break
+    case 11:
+        namaBulan = "November"
+    break
+    case 12:
+        namaBulan = "Desember"
+}
+//membuat bulan daari 05 menjadi mei
+console.log(namaBulan)
+
+//harapan ttl dengan urutan berbeda
+let newTTl =
+[ttl[2], ttl[0], ttl[1]]
+console.log(newTTl)
+
+let newttl = ttl.join("-")
+console.log(newttl)
+
+let batasi15 = input[1].slice(0, 15)
+console.log(batasi15)
+}
+
+dataHandling2(input);
+
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah //batasi hanya 15 karakter saja pada array elemen ke 2
+ */
